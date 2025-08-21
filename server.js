a0cd8f81fd7b69fdf12fb167a01f4ffe2f86b50b
ziheng.zhou <ziheng.zhou@bytedance.com>
2025-08-21 14:29:34 +0800

    feat: enable prod ports

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ wss.on('connection', ws => {
   });
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
